7d1fa697f357515801dca26f1f05cc299bfd5e99
Zarauto <alvarogbecker@gmail.com>
2024-02-01 14:44:39 -0300

    Cria enums de ingredientes e receitas

---
+++ src/recipe/Ingredient.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace recipe {
+enum class Ingredient : int {  // NOLINT
+    BOTTOMBUN = 0,
+    BEEFPATTY = 1,
+    VEGPATTY = 2,
+    KETCHUP = 3,
+    MUSTARD = 4,
+    LETTUCE = 5,
+    TOMATO = 6,
+    CHEESE = 7,
+    EGG = 8,
+    BACON = 9
+};
+}
---
+++ src/recipe/Recipe.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "Ingredient.hpp"
+
+namespace recipe {
+enum class RecipeName : int {  
+    BLT = 0,
+    CHEESEBURGER = 1,
+    VEGAN = 2,
+    MEATLOVER = 3,
+    SPECIAL = 4
+};
+
+
+
+}
