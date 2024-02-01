fc6fedff896e30e579fbcb0ed2efa27842b050f3
Zarauto <alvarogbecker@gmail.com>
2024-02-01 17:27:55 -0300

    A

--- src/entity/KetchupBottle.cpp
+++ src/entity/KetchupBottle.cpp
@@ -4,8 +4,6 @@
 
 entity::KetchupBottle::KetchupBottle(glm::vec3 position) : Entity(position, glm::vec3(0.2)), normal_position(position){
 
-    //this->normal_position = position;
-
     for (size_t i = 0; i < GetObj().bbox_min_.size(); ++i) {
         glm::vec3 bbox_min = GetObj().bbox_min_[i] * scale_;
         glm::vec3 bbox_max = GetObj().bbox_max_[i] * scale_;
@@ -16,7 +14,6 @@ entity::KetchupBottle::KetchupBottle(glm::vec3 position) : Entity(position, glm:
 }
 
 void entity::KetchupBottle::Update(double delta) { 
-    printf("%f\n", this->normal_position.y);
 
     input::Mouse mouse = Input::Instance().mouse_;
     if(! mouse.M1)
