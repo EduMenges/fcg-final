0454186406a93fd311bd64319d4692588470b246
Zarauto <alvarogbecker@gmail.com>
2024-02-02 14:51:56 -0300

    Vincula EIngredient a cada ingrediente

--- src/entity/ingredient/Mustard.hpp
+++ src/entity/ingredient/Mustard.hpp
@@ -6,6 +6,7 @@ namespace ingredient {
 class Mustard : public Ingredient {
    public:
     explicit Mustard(glm::vec3 position);
+    const recipe::EIngredient index = recipe::EIngredient::MUSTARD;
 
     void Update(double delta) override;
 
--- src/entity/ingredient/TopBun.hpp
+++ src/entity/ingredient/TopBun.hpp
@@ -6,6 +6,7 @@ namespace ingredient {
 class TopBun : public Ingredient {
    public:
     explicit TopBun(glm::vec3 position);
+    const recipe::EIngredient index = recipe::EIngredient::TOPBUN;
 
     void Update(double delta) override;
 
