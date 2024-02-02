5246653240d9cae0bc4c1fe3dc8775f884564370
Zarauto <alvarogbecker@gmail.com>
2024-02-02 16:25:52 -0300

    Top Bun

--- src/entity/ingredient/TopBun.cpp
+++ src/entity/ingredient/TopBun.cpp
@@ -14,6 +14,6 @@ ingredient::TopBun::TopBun(glm::vec3 position) : Ingredient(position, glm::vec3(
 void ingredient::TopBun::Update(double delta) {}
 
 Obj& ingredient::TopBun::GetObj() {
-    static Obj obj("../../../data/ingredients/ketchup-splash.obj");
+    static Obj obj("../../../data/ingredients/top_bun.obj");
     return obj;
 }
