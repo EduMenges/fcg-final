ea0b73d1c257df16f65d0601bc4547a2bd6de49d
Eduardo Menges Mattje <eduardomattje@gmail.com>
2024-02-02 11:58:14 -0300

    Fixed order and bezier compiling on Linux

--- src/scene/Menu.cpp
+++ src/scene/Menu.cpp
@@ -10,7 +10,7 @@ scene::Menu::Menu() : Scene({}, {}) {
     entities_.emplace_back(std::make_unique<entity::Table>(glm::vec3{3, .8, 2}));
     entities_.emplace_back(std::make_unique<entity::KetchupBottle>(glm::vec3{3, 1.7, 2}));
     //entities_.emplace_back(std::make_unique<entity::KetchupBottle>(glm::vec3{3, 1.7, 3}));
-    //entities_.emplace_back(std::make_unique<entity::Egg>(glm::vec3{3, 1.7, 4}));
+    entities_.emplace_back(std::make_unique<entity::Egg>(glm::vec3{3, 1.7, 4}));
 
     player_.SetPosition({3.0F, 2.0F, 3.5F, 1.0F});
     camera_->SetRotation({-2.45, 0.45});
