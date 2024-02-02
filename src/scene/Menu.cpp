151a29e04332415e2e146daa71b80da2baeb035e
Eduardo Menges Mattje <eduardomattje@gmail.com>
2024-02-02 11:58:14 -0300

    Fixed order and bezier compiling on Linux

--- src/scene/Menu.cpp
+++ src/scene/Menu.cpp
@@ -6,7 +6,6 @@
 #include "entity/ingredient/IngredientImports.hpp"
 #include "Player.hpp"
 #include "singleton/Input.hpp"
-#include "entity/FlyingKetchup.hpp"
 
 scene::Menu::Menu() : Scene({}, {}) {
     entities_.emplace_back(std::make_unique<entity::Table>(glm::vec3{3, .8, 2}));
