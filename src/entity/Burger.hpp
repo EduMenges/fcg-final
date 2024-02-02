73bd19e755011337ea55a6b473310580d57f0837
Zarauto <alvarogbecker@gmail.com>
2024-02-02 20:30:37 -0300

    Adiciona ingrediente ao burger (falta ajustar altura)

--- src/entity/Burger.hpp
+++ src/entity/Burger.hpp
@@ -3,6 +3,7 @@
 //#include "Entity.hpp"
 #include "ingredient/Ingredient.hpp"
 #include <vector>
+#include <memory>
 
 namespace entity {
 
@@ -11,10 +12,16 @@ class Burger : public Entity {
     explicit Burger(glm::vec3 position);
 
     std::vector<std::unique_ptr<ingredient::Ingredient>> ingredients = {};
+    float yOffset;
+    bool isComplete;
 
     void Update(double delta) override;
 
-    void Draw(Camera& c) override;
+    void Draw(Camera& c) override; 
+
+    void AddIngredient(recipe::EIngredient index);
+
+    std::unique_ptr<ingredient::Ingredient> GetIngredientByIndex(recipe::EIngredient index);
 
     Obj& GetObj() override;
 };
