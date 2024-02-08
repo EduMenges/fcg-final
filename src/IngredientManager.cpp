7bfdae538178d42e696e81a26dfff4b59c866c38
Eduardo Menges Mattje <eduardomattje@gmail.com>
2024-02-08 10:05:12 -0300

    Added rotation animation for the ingredients when they're being looked at

--- src/IngredientManager.cpp
+++ src/IngredientManager.cpp
@@ -6,79 +6,56 @@
 IngredientManager::IngredientManager(EntityContainer* ec, HeldObject* ho) : entities_(ec), held_object_(ho) {}
 
 void IngredientManager::Init() {
-    entities_->emplace_back(std::make_unique<ingredient::Lettuce>(glm::vec3{1.2, table_y, -2})); //
+    entities_->emplace_back(std::make_unique<ingredient::Lettuce>(glm::vec3{1.2, table_y, -2}));  //
     lettuce_ref = dynamic_cast<ingredient::Lettuce*>(entities_->back().get());
 
-    entities_->emplace_back(std::make_unique<ingredient::Egg>(glm::vec3{-2, table_y, -0.7})); //
+    entities_->emplace_back(std::make_unique<ingredient::Egg>(glm::vec3{-2, table_y, -0.7}));  //
     egg_ref = dynamic_cast<ingredient::Egg*>(entities_->back().get());
 
-    entities_->emplace_back(std::make_unique<ingredient::Tomato>(glm::vec3{-1.2, table_y, -2})); //
+    entities_->emplace_back(std::make_unique<ingredient::Tomato>(glm::vec3{-1.2, table_y, -2}));  //
     tomato_ref = dynamic_cast<ingredient::Tomato*>(entities_->back().get());
 
-    entities_->emplace_back(std::make_unique<ingredient::Bacon>(glm::vec3{2, table_y, -0.7})); //
+    entities_->emplace_back(std::make_unique<ingredient::Bacon>(glm::vec3{2, table_y, -0.7}));  //
     bacon_ref = dynamic_cast<ingredient::Bacon*>(entities_->back().get());
 
-    entities_->emplace_back(std::make_unique<ingredient::Cheese>(glm::vec3{2, table_y, 0.7})); //
+    entities_->emplace_back(std::make_unique<ingredient::Cheese>(glm::vec3{2, table_y, 0.7}));  //
     cheese_ref = dynamic_cast<ingredient::Cheese*>(entities_->back().get());
 
-    entities_->emplace_back(std::make_unique<ingredient::BeefPatty>(glm::vec3{-2, table_y, 0.7})); //
+    entities_->emplace_back(std::make_unique<ingredient::BeefPatty>(glm::vec3{-2, table_y, 0.7}));  //
     beef_ref = dynamic_cast<ingredient::BeefPatty*>(entities_->back().get());
 
-    entities_->emplace_back(std::make_unique<ingredient::TopBun>(glm::vec3{0, table_y, 2})); //
+    entities_->emplace_back(std::make_unique<ingredient::TopBun>(glm::vec3{0, table_y, 2}));  //
     bun_ref = dynamic_cast<ingredient::TopBun*>(entities_->back().get());
 
-    entities_->emplace_back(std::make_unique<ingredient::KetchupBottle>(glm::vec3{1.2, table_y, 2})); 
-    ketchup_ref = dynamic_cast<ingredient::KetchupBottle*>(entities_->back().get());
+    entities_->emplace_back(std::make_unique<ingredient::KetchupBottle>(glm::vec3{1.2, table_y, 2}));
+    ketchup_ref              = dynamic_cast<ingredient::KetchupBottle*>(entities_->back().get());
     ketchup_ref->rotation_.y = glm::radians(180.0F);
 
-    entities_->emplace_back(std::make_unique<ingredient::MustardBottle>(glm::vec3{-1.2, table_y, 2})); 
-    mustard_ref = dynamic_cast<ingredient::MustardBottle*>(entities_->back().get());
+    entities_->emplace_back(std::make_unique<ingredient::MustardBottle>(glm::vec3{-1.2, table_y, 2}));
+    mustard_ref              = dynamic_cast<ingredient::MustardBottle*>(entities_->back().get());
     mustard_ref->rotation_.y = glm::radians(180.0F);
 }
 
-void IngredientManager::Update(double /* delta */) {
+void IngredientManager::Update(double delta) {
+    const std::array<ingredient::Ingredient*, kIngredientAmount> kIngredients{
+        lettuce_ref, egg_ref,    tomato_ref, bacon_ref,   cheese_ref,
+        beef_ref,    cheese_ref, bun_ref,    ketchup_ref, mustard_ref};
+
     input::Mouse mouse = Input::Instance().mouse_;
+
     bool grab = Input::Instance().IsOn(GLFW_KEY_E);
 
-    // No caso de clicar com M1 para pegar um ingrediente e não estar segurando nenhum
-    if(held_object_->object_== nullptr && (mouse.M1 || grab)){
+    const bool kInteracted         = mouse.M1 || grab;
+    const bool kNotHoldingAnything = held_object_->object_ == nullptr;
+
+    for (auto ingredient : kIngredients) {
         Camera& camera = *(held_object_->camera);
 
-        if(lettuce_ref->IsBeingLooked(camera)){
-            held_object_->Set(*lettuce_ref);
-        }
-
-        else if(egg_ref->IsBeingLooked(camera)){
-            held_object_->Set(*egg_ref);
-        }
-
-        else if(tomato_ref->IsBeingLooked(camera)){
-            held_object_->Set(*tomato_ref);
-        }
-
-        else if(bacon_ref->IsBeingLooked(camera)){
-            held_object_->Set(*bacon_ref);
-        }
-        
-        else if(cheese_ref->IsBeingLooked(camera)){
-            held_object_->Set(*cheese_ref);
-        }
-
-        else if(beef_ref->IsBeingLooked(camera)){
-            held_object_->Set(*beef_ref);
-        }
-
-        else if(bun_ref->IsBeingLooked(camera)){
-            held_object_->Set(*bun_ref);
-        }
-
-        else if(ketchup_ref->IsBeingLooked(camera)){
-            held_object_->Set(*ketchup_ref);
-        }
-
-        else if(mustard_ref->IsBeingLooked(camera)){
-            held_object_->Set(*mustard_ref);
+        if (ingredient->IsBeingLooked(camera)) {
+            ingredient->rotation_.y += delta;
+            if (kNotHoldingAnything && kInteracted) {
+                held_object_->Set(*ingredient);
+            }
         }
     }
-
 }
--- src/IngredientManager.hpp
+++ src/IngredientManager.hpp
@@ -10,6 +10,8 @@
 class IngredientManager {
    public:
     static constexpr float kY = 0.0f;
+    static constexpr size_t kIngredientAmount = 10;
+
     using EntityContainer = std::list<std::unique_ptr<Entity>>;
 
     explicit IngredientManager(EntityContainer* ec, HeldObject* ho);
@@ -21,13 +23,13 @@ class IngredientManager {
     EntityContainer* entities_;
     HeldObject*      held_object_;
 
-    ingredient::Lettuce*   lettuce_ref;
-    ingredient::Egg*       egg_ref;
-    ingredient::Tomato*    tomato_ref;
-    ingredient::Cheese*    cheese_ref;
-    ingredient::Bacon*     bacon_ref;
-    ingredient::BeefPatty* beef_ref;
-    ingredient::TopBun*    bun_ref;
+    ingredient::Lettuce*       lettuce_ref;
+    ingredient::Egg*           egg_ref;
+    ingredient::Tomato*        tomato_ref;
+    ingredient::Cheese*        cheese_ref;
+    ingredient::Bacon*         bacon_ref;
+    ingredient::BeefPatty*     beef_ref;
+    ingredient::TopBun*        bun_ref;
     ingredient::KetchupBottle* ketchup_ref;
     ingredient::MustardBottle* mustard_ref;
 
